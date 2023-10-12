0f0f87a9af29797ed41ae574a4db7a8cabe1db25
Eke Péter <peterekepeter@gmail.com>
2023-10-13 00:03:12 +0200

    Some wrapper classes

--- src/x11.zig
+++ src/x11.zig
@@ -7,50 +7,17 @@ const x = @cImport({
 
 pub fn main() void 
 {
-    var c = x.xcb_connect(null, null);
-    // defer x.xcb_disconnect(c);
-    var screen = x.xcb_setup_roots_iterator (x.xcb_get_setup (c)).data;
-    var depth = screen.*.root_depth;
-    std.debug.print("depth {}\n", .{depth});
-    var colormap = screen.*.default_colormap;
-    _ = colormap;
+    var xc = X11Connection.init();
+    defer xc.deinit();
+    var c = xc.conn;
 
-    var width: u16 = 600;
-    var height: u16 = 400;
+    var win = X11Window.init(&xc, .{
+        .title = "Test Window"
+    });
 
-    var win = x.xcb_generate_id(c);
-    var windowResult = x.xcb_create_window(
-        c,                            
-        x.XCB_COPY_FROM_PARENT,        
-        win,                         
-        screen.*.root,                
-        0, 0,                   
-        width, height,                   
-        10,                         
-        x.XCB_WINDOW_CLASS_INPUT_OUTPUT,
-        screen.*.root_visual,       
-        x.XCB_CW_BACK_PIXEL | x.XCB_CW_EVENT_MASK, 
-        &[_]u32{
-            screen.*.white_pixel,
-            x.XCB_EVENT_MASK_EXPOSURE       | x.XCB_EVENT_MASK_BUTTON_PRESS   |
-              x.XCB_EVENT_MASK_BUTTON_RELEASE | x.XCB_EVENT_MASK_POINTER_MOTION |
-              x.XCB_EVENT_MASK_ENTER_WINDOW   | x.XCB_EVENT_MASK_LEAVE_WINDOW   |
-              x.XCB_EVENT_MASK_KEY_PRESS      | x.XCB_EVENT_MASK_KEY_RELEASE,
-        },
-    );
-    std.debug.print("window created, seq {}\n", .{ windowResult.sequence } );            
-    const title = "Hello World!";
-    _ = x.xcb_change_property (c, x.XCB_PROP_MODE_REPLACE, win,
-        x.XCB_ATOM_WM_NAME, x.XCB_ATOM_STRING, 8,
-        title.len, title);
-    var black = x.xcb_generate_id(c);
-    _ = x.xcb_create_gc (c, black, win, x.XCB_GC_FOREGROUND, &[_]u32{ screen.*.black_pixel });
-
-    var empty_gc = x.xcb_generate_id(c);
-    _ = x.xcb_create_gc(c, empty_gc, win, 0, null);
 
     {
-        const mapResult = x.xcb_map_window(c, win);
+        const mapResult = x.xcb_map_window(c, win.win);
         std.debug.print("map seq {}\n", .{ mapResult.sequence });
         const flushResult = x.xcb_flush (c);
         std.debug.print("flush result {}\n", .{ flushResult });
@@ -63,11 +30,11 @@ pub fn main() void
     renderXorTextureToCanvas(pixmap_width, pixmap_height, &pix_data, frame);
     
     var pixmap = x.xcb_generate_id(c);
-    _ = x.xcb_create_pixmap(c,depth,pixmap,win,pixmap_width,pixmap_height);
+    _ = x.xcb_create_pixmap(c,xc.depth,pixmap,win.win,pixmap_width,pixmap_height);
     var format = x.XCB_IMAGE_FORMAT_Z_PIXMAP;
 
-    var image = x.xcb_image_create_native(c,pixmap_width,pixmap_height,@intCast(format),depth,null,pix_data.len,&pix_data);
-    _ = x.xcb_image_put(c, pixmap, empty_gc, image, 0, 0, 0);
+    var image = x.xcb_image_create_native(c,pixmap_width,pixmap_height,@intCast(format),xc.depth,null,pix_data.len,&pix_data);
+    _ = x.xcb_image_put(c, pixmap, win.gc, image, 0, 0, 0);
     _ = x.xcb_image_destroy(image);
 
     var exitRequested = false;
@@ -81,7 +48,7 @@ pub fn main() void
         std.debug.print("event received type {}\n", .{ event.response_type });
         switch (event.response_type){
             x.XCB_EXPOSE => {
-                flushPixmap(c, win, empty_gc, pixmap, pixmap_width, pixmap_height, format, depth, &pix_data);
+                flushPixmap(c, win.win, win.gc, pixmap, pixmap_width, pixmap_height, format, xc.depth, &pix_data);
             },
             x.XCB_KEY_PRESS => {
                 const keyEventPtr = @as([*c]x.xcb_key_press_event_t, @ptrCast(eventPtr));
@@ -92,7 +59,7 @@ pub fn main() void
                 else {
                     frame += 1;
                     renderXorTextureToCanvas(pixmap_width, pixmap_height, &pix_data, frame);
-                    flushPixmap(c, win, empty_gc, pixmap, pixmap_width, pixmap_height, format, depth, &pix_data);
+                    flushPixmap(c, win.win, win.gc, pixmap, pixmap_width, pixmap_height, format, xc.depth, &pix_data);
                     std.debug.print("frame {}\n", .{frame});
                 }
             },
@@ -120,6 +87,100 @@ fn renderXorTextureToCanvas(pixmap_width: u16, pixmap_height: u16, pix_data: []u
         }
     }
 }
+
+const X11Connection = struct {
+    conn: ?*x.xcb_connection_t,
+    screen: *x.xcb_screen_t,
+    depth: u8,
+    colormap: u32,
+    
+    fn init() X11Connection {
+        var c = x.xcb_connect(null, null);
+        var screen = x.xcb_setup_roots_iterator (x.xcb_get_setup (c)).data;
+        var depth = screen.*.root_depth;
+        var colormap = screen.*.default_colormap;
+        return X11Connection {
+            .conn = c,
+            .screen = screen,
+            .depth = depth,
+            .colormap = colormap,
+        };
+    }
+
+    fn deinit(self: *X11Connection) void {
+        x.xcb_disconnect(self.conn);
+        self.conn = null;
+    }
+};
+
+const X11WindowInitOptions = struct {
+    width: u16 = 600,
+    height: u16 = 400,
+    title: []const u8 = "Window",
+};
+
+const X11Window = struct {
+    win: u32,
+    xc: *X11Connection,
+    gc: u32,
+    
+    fn init(xc: *X11Connection, opt: X11WindowInitOptions) X11Window
+    {
+        var c = xc.conn;
+        var win = x.xcb_generate_id(c);
+        var screen = xc.screen;
+        var windowResult = x.xcb_create_window(
+            c,                            
+            x.XCB_COPY_FROM_PARENT,        
+            win,                         
+            screen.*.root,                
+            0, 0,                   
+            opt.width, opt.height,                   
+            10,                         
+            x.XCB_WINDOW_CLASS_INPUT_OUTPUT,
+            screen.*.root_visual,       
+            x.XCB_CW_BACK_PIXEL | x.XCB_CW_EVENT_MASK, 
+            &[_]u32{
+                screen.*.white_pixel,
+                x.XCB_EVENT_MASK_EXPOSURE       | x.XCB_EVENT_MASK_BUTTON_PRESS   |
+                x.XCB_EVENT_MASK_BUTTON_RELEASE | x.XCB_EVENT_MASK_POINTER_MOTION |
+                x.XCB_EVENT_MASK_ENTER_WINDOW   | x.XCB_EVENT_MASK_LEAVE_WINDOW   |
+                x.XCB_EVENT_MASK_KEY_PRESS      | x.XCB_EVENT_MASK_KEY_RELEASE,
+            },
+        );
+        _ = windowResult;
+
+        var empty_gc = x.xcb_generate_id(c);
+        _ = x.xcb_create_gc(c, empty_gc, win, 0, null);
+
+
+        const title = opt.title;
+        _ = x.xcb_change_property (c, x.XCB_PROP_MODE_REPLACE, win,
+            x.XCB_ATOM_WM_NAME, x.XCB_ATOM_STRING, 8,
+            @intCast(title.len), title.ptr);
+
+        return X11Window {
+            .win = win,
+            .xc = xc,
+            .gc = empty_gc,
+        };
+    }
+
+    fn deinit(w: *X11Window) void {
+        x.xcb_destroy_window(w.xc.conn, w.win);
+    }
+};
+
+const XWindow = struct {
+    win: u32,
+    default_gc: u32,
+    pixmap: u32,
+    pixmap_width: u16,
+    pixmap_height: u16,
+    format: c_int,
+    depth: u8,
+    pix_data: []u8,
+};
 
 fn flushPixmap(c: ?*x.xcb_connection_t, win: u32, gc: u32, pixmap: u32, pixmap_width: u16, pixmap_height: u16, format: c_int, depth: u8, pix_data: []u8) void {
     var image = x.xcb_image_create_native(c,pixmap_width,pixmap_height,@intCast(format),depth,null,@intCast(pix_data.len),pix_data.ptr);
